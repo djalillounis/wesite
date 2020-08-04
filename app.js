3c6280fde17639d05b80917ea76409292273d5ef
djalillounis <djalillounis3@gmail.com>
2020-08-04 20:32:22 +0200

    V1

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+
+var path = require('path')
+
+
+
+const port = 5555
+
+app.get('/', (req, res) {
+
+        res.sendFile('index.html', {root:path.join(__dirname,'./public')})
+
+
+
+})
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
